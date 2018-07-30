05d98ded9a864775db31e1113c7445002c71e72a
sijia zhu <sijiazhu@sijias-iMac.local>
2018-07-30 14:57:00 -0700

    push to heroku try8

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin",
-        "*");
+        "https://web-summer2018-nodejs-kenzhu.herokuapp.com");
     res.header("Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods",
